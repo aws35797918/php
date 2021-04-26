f59205cc0b7f046398b29588a689fd0c50913065
Laptop <aws35797918@gmail.con>
2021-04-26 09:16:49 +0800

    4/25-1

--- MemberLogin.php
+++ MemberLogin.php
@@ -7,7 +7,38 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include("header.php");?>
+    <?php include("header.php");
+        if(isset($_POST['account']))
+        {
+            
+            include("dblink.php");
+            $ac=$_POST['account'];
+            $pw=$_POST['password'];
+            $sql="SELECT Password FROM member WHERE Account='$ac'";
+            $result=mysqli_query($link,$sql);
+            if(mysqli_num_rows($result)>0)
+            {
+                
+                while($row = mysqli_fetch_array($result))
+                {
+                    if($row[0]==base64_encode($pw))
+                    {
+                        setcookie("user",$ac,time()+3600);
+                        header("Location:BuyList.php");
+
+                    }
+                    else
+                    {
+                        echo"<script>alert('密碼錯誤')</script>";
+
+                    }
+                }
+                
+            }
+            else{echo"<script>alert('查無此帳號')</script>";}
+        }
+
+    ?>
     
         
 
@@ -15,7 +46,7 @@
         <div id="logindiv">
 
 
-            <form action="ManagementProduct.php" method="post">
+            <form action="MemberLogin.php" method="post">
                 <h1>會員登入</h1>
                 <label for="account">▲</label><input class="login" type="text" placeholder="帳號" name="account"><br>
                 <label for="password">▲</label><input class="login" type="password"placeholder="密碼"  name="password"><br>
